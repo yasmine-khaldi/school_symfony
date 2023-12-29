173e1a833131f02843c3397cc0779a267d56fc74
yasmine-khaldi <khaldiyasmine27@gmail.com>
2023-12-30 00:20:15 +0100

    first commit

---
+++ assets/bootstrap.js
@@ -0,0 +1,5 @@
+import { Controller } from '@hotwired/stimulus';
+
+const app = Controller();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
